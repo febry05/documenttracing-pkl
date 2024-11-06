ddae5885d61d36fc6f68d0d09879f49b71333c72
ferdyuuya <muhammadferdy7404@gmail.com>
2024-11-06 11:03:07 +0800

    refactor: Update route definitions for dashboard and user permissions; enhance middleware grouping for better access control

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,21 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route::middleware(['auth'])->group(function () {
+
 Route::middleware(['auth', 'role:Administrator'])->group(function () {
+    //User
     Route::resource('/users', UserController::class);
     //Master
     Route::resource('/master/user-roles', RoleController::class);
-    Route::resource('/master/permissions', PermissionController::class);
+    Route::resource('/master/user-permissions', PermissionController::class);
     Route::resource('/master/user-positions', UserPositionController::class);
     Route::resource('/master/user-divisions', UserDivisionController::class);
     Route::resource('/master/project-business-types', ProjectBusinessTypeController::class);
