278ce910fd787e1dd98aaf2ee50a0fb17c030498
Muhammad Azhim Nugroho <mazhn34@gmail.com>
2025-01-03 15:48:13 +0800

    refactor: fixed checkPermission middleware

--- app/Http/Middleware/checkPermission.php
+++ app/Http/Middleware/checkPermission.php
@@ -18,22 +18,22 @@ class checkPermission
     public function handle(Request $request, Closure $next, $handleOwnedProject = false, $permission): Response
     {
         $user = User::find(Auth::user()->id);
-
-        // Contoh penggunaan middleware di route:
-        // ->middleware('check_permission:true,View Project Document Version');
-        // Artinya, si user harus bisi 'HandleOwnedProject' lwn 'View Project Document Version' permission supaya bisa ngakses, t
-        //
-        // kalo kd handak makai 'Handle Owned Project', ganti `true` jadi `false`
-
         // dd($handleOwnedProject === true && !$user->can('Handle Owned Project'));
         // dd('handleOwnedProject: ' . $handleOwnedProject, 'permission: ' . $permission, 'User have Handle Owned Project permission: ' . $user->can('Handle Owned Project'), 'User have permission: ' . $user->can($permission));
-        if($handleOwnedProject === false && !$user->can('Handle Owned Project')) {
-            abort(403, 'Unauthorized action. Only users with \'Handle Owned Project \' permission can access this resource.');
-        }
 
-        if(!$user->can($permission)) {
+        // ini jalan kalo:
+        //->middleware('check_permission:true,View Project Document Version');
+        // ditulis
+        if (!($handleOwnedProject === true && $user->hasAny(['Handle Owned Project', $permission]))) {
+            abort(403, 'Unauthorized action. Only users with \'Handle Owned Project \' or ' . $permission . ' permission can access this resource.');
+
+        // ini jalan kalo:
+        //->middleware('check_permission:false,View Project Document Version');
+        // ditulis
+        } else if (!$user->can($permission)) {
             abort(403, 'Unauthorized action. Only users with ' . $permission . ' permission can access this resource.');
         }
+
         return $next($request);
     }
 }
